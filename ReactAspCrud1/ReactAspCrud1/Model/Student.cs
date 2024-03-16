562626085a311e3836641d699c14e0c382753f77
SeemaGaikwadDeveloper <simagaikwad1234@gmail.com>
2024-03-16 21:56:20 +0100

    second commit

---
+++ ReactAspCrud1/ReactAspCrud1/Model/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactAspCrud1.Model
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+        public string stname { get; set; }
+        public string course { get; set; }
+    }
+}
---
+++ ReactAspCrud1/ReactAspCrud1/Model/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ReactAspCrud1.Model
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=StudentDB;Integrated Security=True; TrustServerCertificate=True");
+        }
+    }
+}
